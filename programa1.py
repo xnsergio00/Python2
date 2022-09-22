8b04d31bb70071db9fed5268354bf177a523f078
ciberseguranca <sergioduartesa@enta.pt>
2022-09-22 12:10:09 +0000

    Implementa varias operações aritmeticas

--- programa1.py
+++ programa1.py
@@ -31,7 +31,8 @@ def aritmetica(valor1, valor2, op='+'):
 if __name__ == "__main__":
     fator1 = 10
     fator2 = 20
-    print(f"{fator1} mais {fator2} = {aritmetica(fator1, fator2, '*')}")
+    operacao = '-'
+    print(f"{fator1} {operacao} {fator2} = {aritmetica(fator1, fator2, operacao)}")
 
 
 
