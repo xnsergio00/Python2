cc0ab7aa4c112fb0a40dd892cf81adc255677e35
ciberseguranca <sergioduartesa@enta.pt>
2022-09-23 10:29:22 +0000

    Diz se é par ou impar

--- programa2.py
+++ programa2.py
@@ -2,6 +2,32 @@
 ##(A, 80-100; B, 60-79; C, 40-59; D, 20-39; E, 0-19)
 
 
-nota = input ('Insere a tua nota: ')
-if nota > 79 :
-    print('Tens A')
+if __name__ == "__main__":
+    nome = input('Como te chamas?')
+
+while True:
+    nota = int(input('Insere a tua nota: '))
+
+    if 80 <= nota <= 100:
+        print('Tens A')
+
+    if 60 <= nota <= 79:
+        print('Tens B')
+
+    if 40 <= nota <= 59:
+        print('Tens c')
+
+    if 20 <= nota <= 39:
+        print('Tens D')
+
+    if 0 <= nota <= 19:
+        print('Tens E')
+
+    if 0 < nota > 100:
+        print('Número Inválido')
+
+    continuar = input('Quer continuar? [s,n] ')
+    if continuar == 'n':
+        break
+
+print(f'Adeus {nome}!')
---
+++ programa3.py
@@ -0,0 +1,34 @@
+
+def par(numero):
+    if num % 2 == 0:
+
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    nome = input('Como te chamas?')
+    continuar = 's'
+    while continuar == 's':
+        num = int(input('Insira um número'))
+        if par(num):
+            print(f'O número {num} é par.')
+        else:
+            print(f'O número {num} é impar.')
+        continuar = input('Repetir [s | n]? ')
+    print(f'Adeus {nome}!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
