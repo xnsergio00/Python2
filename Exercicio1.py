b896b058ae074c0dd86177fc2d3bde5802a0e2c7
ciberseguranca <sergioduartesa@enta.pt>
2022-09-23 12:10:51 +0000

    Exercicio

---
+++ Exercicio1.py
@@ -0,0 +1,12 @@
+"""
+1. Pede ao utilizador 2 números. O 2º número deve ser >= 1º
+Mostra todos os números primos, que há entre o 1º e o 2º
+Depois de mostrar os números diz quantos números primos havia.
+
+2. Pede ao utilizador um número inicial
+Pede ao utilizador um número que representa "Quantos"
+Mostra aquela quantidade de números primos a partir do número inicial
+"""
+
+
+def
